d5f54257bb5529a3cca24c215068da1a33059538
Sergei Tyshchenko <styshchenko4@gmail.com>
2019-07-24 19:56:19 +0600

    Removed another folders

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// let asideMenu = document.querySelector("#drawers-menu");
+// let drawer = document.querySelector("nav .drawer");
+// let drawerPosition = false;
+// let backdrop = document.querySelector(".backdrop");
+
+// asideMenu.addEventListener("click", function() {
+//   drawerPosition = !drawerPosition;
+
+//   if (drawerPosition) {
+//     drawer.style.transform = "translateX(0)";
+//     backdrop.style.display = "block";
+//   } else {
+//     drawer.style.transform = "translate(-100%)";
+//   }
+// });
+
+// backdrop.addEventListener("click", function() {
+//   drawer.style.transform = "translate(-100%)";
+//   backdrop.style.display = "none";
+// });
+
+
+
+
